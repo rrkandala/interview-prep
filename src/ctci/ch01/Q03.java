50494a7cf30485785f23b9b79d5c814750fed8ba
izzy moriguchi <izzymoriguchi@berkeley.edu>
2017-08-08 10:02:59 -0700

    Add my solution for problem 1_3

---
+++ src/ctci/ch01/Q03.java
@@ -0,0 +1,32 @@
+package ctci.ch01;
+
+/**
+ * Created by izzymoriguchi on 8/8/17.
+ */
+public class Q03 {
+    public String URLify(String str, int trueSize) {
+        // count the number of space in string
+        int count = 0;
+        for (int i = 0; i < trueSize; i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        // 3 represents the length of ‘%20’
+        int lenOfOutput = trueSize + count * 3 - count;
+        char[] output = new char[lenOfOutput];
+        int k = 0;
+        for (int i = 0; i < trueSize; i++) {
+            if (str.charAt(i) == ' ') {
+                output[k] = '%';
+                output[k + 1] = '2';
+                output[k + 2] = '0';
+                k = k + 3;
+            } else {
+                output[k] = str.charAt(i);
+                k++;
+            }
+        }
+        return new String(output);
+    }
+}
