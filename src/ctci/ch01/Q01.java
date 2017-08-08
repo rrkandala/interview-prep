2742d1f4280b16535592a19cc1e8a77c5108e3d3
izzy moriguchi <izzymoriguchi@berkeley.edu>
2017-08-08 09:29:17 -0700

    Add my solution for problem 1_1

---
+++ src/ctci/ch01/Q01.java
@@ -0,0 +1,22 @@
+package ctci.ch01;
+
+/**
+ * Created by izzymoriguchi on 8/8/17.
+ */
+public class Q01 {
+    public static boolean isUnique(String word) {
+        if (word.length() <= 0 || word.length() > 128) {
+            return false;
+        }
+
+        boolean[] setOfChars = new boolean[128];
+        for (int i = 0; i < word.length(); i++) {
+            int unicode = (int) word.charAt(i);
+            if (setOfChars[unicode]) {
+                return false;
+            }
+            setOfChars[unicode] = true;
+        }
+        return true;
+    }
+}
